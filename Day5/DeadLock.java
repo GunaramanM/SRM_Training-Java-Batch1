2d85f8c6e043ca68de592de0306c3a42e6fa3455
GunaramanM <86225498+GunaramanM@users.noreply.github.com>
2021-06-26 14:12:41 +0530

    Update and rename a.java to DeadLock.java

---
+++ Day5/DeadLock.java
@@ -0,0 +1,58 @@
+package Day5;
+public class DeadLock {
+
+	public static void main(String[] args) throws Exception
+	{
+		 String resource1 = "resourceone";  
+	     String resource2 = "resourcetwo";  
+	    
+	    Thread t1 = new Thread()
+	    {  
+	      public void run() 
+	      {  
+	          synchronized (resource1) {  
+	           System.out.println("T1 locked resource 1");  
+	  
+	           try 
+	           {
+	        	   Thread.sleep(100);
+	        	   } 
+	           catch (Exception e) {}  
+	  
+	           synchronized (resource2) 
+	           {  
+	            System.out.println("T1 locked resource 2");  
+	           }  
+	         }  
+	      }  
+	    };  
+	  
+	    
+	    Thread t2 = new Thread() 
+	    {  
+	      public void run() 
+	      {  
+	        synchronized (resource2)
+	        {  
+	          System.out.println("T2 locked resource 1");  
+	  
+	          try
+	          {
+	        	  Thread.sleep(100);
+	        	  } 
+	          catch (Exception e) {}  
+	  
+	          synchronized (resource1) 
+	          {  
+	            System.out.println("T2: locked resource 2");  
+	          }  
+	        }  
+	      }  
+	    };  	      
+	    t1.start();  
+	    t2.start();  
+    
+	
+	}
+
+}
