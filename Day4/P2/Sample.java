dbf0ba2180d34968052e10c1a3b1637a48c50c93
GunaramanM <86225498+GunaramanM@users.noreply.github.com>
2021-06-26 11:03:28 +0530

    Add files via upload

---
+++ Day4/P2/Sample.java
@@ -0,0 +1,16 @@
+package P2;
+import P1.*;
+public class Sample {
+    public static void main(String[] args) {
+      
+        Sample1 guna = new Sample1();
+        guna.display();
+        Sample2 raman = new Sample2();
+        raman.display();
+        Sample3 M = new Sample3();
+        M.display();
+
+    } 
+        
+    
+}
